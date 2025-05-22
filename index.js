e012c3148d02d811538a45017f6c9e0d91a42f9b
pedzmrx <pedromoraes750@gmail.com>
2025-05-22 17:05:41 -0400

    atualizacao code

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 const heroi = "O Herói Pedrao"
-const XP = 12000
+const XP = 5000
 
 if (XP < 1000) {
   console.log(heroi + " está no nível Ferro")
-} else if(XP >= 1001 && XP < 2000) {
+}   else if(XP >= 1000 && XP < 2000) {
     console.log(heroi + " está no nível Bronze");
-} else if (XP >= 2001 && XP < 5000)
+}   else if (XP >= 2000 && XP < 5000) {
     console.log(heroi + " está no nível Prata");
-    else if (XP >= 5001 && XP < 7000)
-        console.log(heroi + " está no nível Ouro");
-    else if (XP >= 7001 && XP < 8000)
-        console.log(heroi + " está no nível Platina");
-    else if (XP >= 8001 && XP < 9000)
-        console.log(heroi + " está no nível Ascedente");
-    else if (XP >= 9001 && XP < 10000)
-        console.log(heroi + " está no nível Imortal");
-    else if (XP >= 10001)
-        console.log(heroi + " está no nível Radiante");
-
+}   else if (XP >= 5000 && XP < 7000) {
+    console.log(heroi + " está no nível Ouro");
+}   else if (XP >= 7000 && XP < 8000) {
+    console.log(heroi + " está no nível Platina");
+}    else if (XP >= 8000 && XP < 9000) {
+    console.log(heroi + " está no nível Ascedente");
+}   else if (XP >= 9000 && XP < 10000) {
+    console.log(heroi + " está no nível Imortal");
+}   else if (XP >= 10000) {
+    console.log(heroi + " está no nível Radiante");
+}
     //&& Simbolo AND = juntamente á .. 
-    // garante que só entra no tal nível entre 1000 e 2000.
+    // garante que só entra no tal entre 1000 e 2000.
     
